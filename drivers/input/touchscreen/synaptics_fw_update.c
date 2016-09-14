c878af9f2cdd68ca0dde50a1580ee2d1e27ae344
Andrew Chant <achant@google.com>
2016-09-14 15:25:23 -0700

    input: synaptics: defer sysfs creation during init
    sysfs entries are created which reference fwu->fwu_work.
    defer the creation of these sysfs entries until the end of the init
    function, after fwu->fwu_work has been initialized.
    
    Change-Id: Ib7d5304ec2990454486e2b1d28b640a174c83d12
    Bug: 31252388
    Signed-off-by: Andrew Chant <achant@google.com>
    [razorloves: Backport to 3.4]

--- drivers/input/touchscreen/synaptics_fw_update.c
+++ drivers/input/touchscreen/synaptics_fw_update.c
@@ -1772,6 +1772,14 @@ static int synaptics_rmi4_fwu_init(struct synaptics_rmi4_data *rmi4_data)
 	fwu->initialized = true;
 	fwu->force_update = FORCE_UPDATE;
 
+#ifdef INSIDE_FIRMWARE_UPDATE
+	fwu->fwu_workqueue = create_singlethread_workqueue("fwu_workqueue");
+	INIT_DELAYED_WORK(&fwu->fwu_work, synaptics_rmi4_fwu_work);
+	queue_delayed_work(fwu->fwu_workqueue,
+			&fwu->fwu_work,
+			msecs_to_jiffies(1000));
+#endif
+
 	retval = sysfs_create_bin_file(&rmi4_data->input_dev->dev.kobj,
 			&dev_attr_data);
 	if (retval < 0) {
@@ -1792,14 +1800,6 @@ static int synaptics_rmi4_fwu_init(struct synaptics_rmi4_data *rmi4_data)
 			goto exit_remove_attrs;
 		}
 	}
-
-#ifdef INSIDE_FIRMWARE_UPDATE
-	fwu->fwu_workqueue = create_singlethread_workqueue("fwu_workqueue");
-	INIT_DELAYED_WORK(&fwu->fwu_work, synaptics_rmi4_fwu_work);
-	queue_delayed_work(fwu->fwu_workqueue,
-			&fwu->fwu_work,
-			msecs_to_jiffies(1000));
-#endif
 
 	init_completion(&remove_complete);
 
